d04f343bbe4c61a07fae957bb8f35e6bd3482912
JansherA <43053017+JansherA@users.noreply.github.com>
2018-09-25 09:24:39 -0400

    Update gameproject.py

--- gameproject.py
+++ gameproject.py
@@ -7,3 +7,26 @@ def holeone():
 		break
 	if onehole == "A":
 		distanceone = random.random()
+		if distanceone < .25:
+			print("Success! You are within 5 feet for an easy putt. Nice birdie.")
+			holetwo()
+		else:
+			holeoneshottwo = input("\n\nOops...Your ball landed in the bunker. \n\nWill you \nA. Forfeit the hole and save your energy for an automatic score of 10 on the hole or \n\nB. Continue for a chance of an 5? Type A or B: \n\n ")
+			while holeoneshottwo != "A" or "B":
+				print("You didn't type A or B! (Mrs. Healey stop it I need a good grade) Try again please.")
+				holeoneshottwo = input("\n\nOops...Your ball landed in the bunker. \n\nWill you \nA. Forfeit the hole and save your energy for an automatic score of 10 on the hole or \n\nB. Continue for a chance of an 5? Type A or B: \n\n ")
+				break
+			if holeoneshottwo == "B":
+				distancetwo = random.random()
+				if distancetwo < .25:
+					print("Good job! You nailed the putt and recovery. At least you scored a 5 and saved yourself from disaster.")
+					holetwo()
+				else:
+					print("Unlucky! You still managed to score a 10 with that horrible recovery from the sand. Might as well have forfeited the hole.")
+					holetwo()
+			elif holeoneshottwo == "A":
+				print("Horrible idea! Why would you conserve your energy? This is golf, not cross country. Drink a redbull and take your score of 10.")
+				holetwo()
+	elif onehole == "B":
+		print("Smart play! You mitigated the risks and secured an easy par 3 without breaking a sweat. However, that also makes you lame.")		
+		holetwo()
