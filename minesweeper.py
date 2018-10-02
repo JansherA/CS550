a2e6c41a8bda4a8e5311bcbef14d728afd5dfe62
JansherA <43053017+JansherA@users.noreply.github.com>
2018-10-02 11:06:09 -0400

    Update minesweeper.py

--- minesweeper.py
+++ minesweeper.py
@@ -2,12 +2,13 @@ import random
 import sys
 row = int(sys.argv[1])
 columns = int(sys.argv[2])
-#adapted from https://stackoverflow.com/questions/40566675/how-to-make-a-board-in-python most code adjusted because of required sys.argv
-def main():
-    print("Board:\n")
+# 6 - 12 adapted from https://stackoverflow.com/questions/40566675/how-to-make-a-board-in-python most code adjusted because of required sys.argv
+def board():
+	print("Board:\n")
 
-    for i in range(row): 
-        print('0'*columns) 
+	for i in range(row): 
+		x = [random.randrange(1, 3, 1) for _ in range(1)]
+		print(*x * columns) 
 
 if __name__ == '__main__':
-    main()
+	board()
