adfe929e4f32835434e2710ad4178e8cf931a165
JansherA <43053017+JansherA@users.noreply.github.com>
2018-11-16 14:59:54 -0500

    Update 10.py

--- 10.py
+++ 10.py
@@ -3,18 +3,11 @@ import cv2
 from PIL import Image, ImageFilter
 import random
 import matplotlib.pyplot as plt
-# orig = cv2.imread('notfried.jpg')
-orig = Image.new('notfried.jpg')
-# RGB = random.randrange(100, 100, 3)
-# HSV = rgb2hsv(RGB)
-# HSV(0, 0, 2) = HSV(0, 0, 2) * 1.2
-# # HSV(0, 0, 2) = HSV(0, 0, 2) + 3
-# # HSV(HSV > 1) = 1
-# RGB = hsv2rgb(HSV)
-conv = cv2.cvtColor(orig, cv2.COLOR_BGR2HSV)
-color = plt.imshow(conv, cmap="binary")
-color.save("color.jpg",JPG)
-fried = cv2.imread('color.jpg')
-# for x in len()
-# HSV = (x, y, z)
-cv2.imshow('fried.jpg', fried)
+
+im_gray = cv2.imread("deepfriedvtwo.jpg", cv2.IMREAD_GRAYSCALE)
+im_color = cv2.applyColorMap(im_gray, cv2.COLORMAP_HSV)
+cv2.imwrite("offfried.jpg",im_color)
+
+
+
+# 1500 x 1000
